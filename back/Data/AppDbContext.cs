cc4617656ce776c47d6137b46d83c1f8d61d3a8b
rafael <rafael201123r@gmail.com>
2025-07-12 20:45:42 -0300

    criando pasta models e configurando data e appsettings

---
+++ back/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using encurtador_url.back.Models;
+
+namespace encurtador_url.back.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<UrlMapping> UrlMappings => Set<UrlMapping>();
+    }
+}
---
+++ back/Models/UrlMapping.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace encurtador_url.Back.Models
+{
+    public class UrlMapping
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Url]
+        public string OriginalUrl { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(10, MinimumLength = 4)]
+        public string ShortCode { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
